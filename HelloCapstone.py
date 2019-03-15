c4cbcb2d2086423b01ce4d5516114e1cfe5889c7
Will1974 <48406849+Will1974@users.noreply.github.com>
2019-03-16 09:22:44 +1000

    Add files via upload
    Capstone project

---
+++ HelloCapstone.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# import pandas library
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+#Hello Capstone Project Course!
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
